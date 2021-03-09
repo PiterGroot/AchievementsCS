e69b1e17129fa008ba36d3227ebbb40fc168f088
PiterGroot <70685433+PiterGroot@users.noreply.github.com>
2021-03-09 15:04:48 +0100

    achievemt

---
+++ les2-Loops/Program.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Loops
+//gold
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = {0, 2, 4, 6, 8, 10 };
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+        
+    }
+}
