17fe67d813b327123c5f9f2290989e8b9cee3df3
PiterGroot <70685433+PiterGroot@users.noreply.github.com>
2021-03-09 14:16:39 +0100

    achievement

---
+++ les-2-LinearSearch/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace CS_lessen
+{
+    //brons Les 2: DataTypes
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[]
+			{
+				"Joe",
+				"Rick",
+				"Sam",
+				"Giovanni",
+				"Ramses",
+				"Laura",
+				"Laurens",
+				"Lydia",
+				"Eric",
+				"Rose"
+			};
+			string nameToFind = "Joe";
+			FindStringIndex(nameToFind, names);
+			Console.ReadKey();
+        }
+		public static void FindStringIndex(string nameToFind, string[] names)
+		{
+			for(int i = 0; i < names.Lenght; i++)
+			{
+				if(names[i] == nameToFind) 
+				{
+					Console.WriteLine($"Joe zit op index: {i}");
+				}
+			}
+		}
+    }
+}
