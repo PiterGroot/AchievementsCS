0299683f65ca0f6d68dda90a9e2723d3b9718885
PiterGroot <70685433+PiterGroot@users.noreply.github.com>
2021-03-09 14:45:32 +0100

    achievement

---
+++ Les1-Functies/Program.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Functie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintArgument("Dit is een argument");
+        }
+        public static void PrintArgument(string argument)
+        {
+            Console.WriteLine(argument);
+        }
+    }
+}
