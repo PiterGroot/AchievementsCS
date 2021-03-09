ab0e4ddafde8d124b9c75929e1bfabecf83acb0f
PiterGroot <70685433+PiterGroot@users.noreply.github.com>
2021-03-09 14:38:29 +0100

    achievement

---
+++ les-1-FunctionReturn/Program.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Functie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine($"RandomNumber = {GetRandomInt()}");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+        public static int GetRandomInt()
+        {
+            Random rand = new Random();
+            return rand.Next(1, 21);
+        }
+    }
+}
