12b32a65c7d8c65efca030a8130499e717f95c4c
PiterGroot <70685433+PiterGroot@users.noreply.github.com>
2021-03-09 15:01:07 +0100

    achiebe,ent

---
+++ les2-Arrays/Program.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Array
+{
+	//silver
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] getallen = { 10, 20, 30, 40, 50, 60 };
+            Console.WriteLine($"{getallen[0] + getallen[1] + getallen[2] + getallen[3] + getallen[4] + getallen[5]}");
+            
+        }
+        
+    }
+}
