27d79d5c7a15df9eac934200ceb4a9e185f3c3bb
PiterGroot <70685433+PiterGroot@users.noreply.github.com>
2021-03-09 14:08:04 +0100

    achievement

---
+++ les-2-Datatypes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace CS_lessen
+{
+    //brons Les 2: DataTypes
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            float b = 0.5f;
+            double c = 1.32142412441d;
+            string d = "Word";
+            char e = 'c';
+            bool f = true;
+            Console.WriteLine($"{a} {b} {c} {d} {e} {f}");
+            Console.ReadKey();
+        }
+    }
+}
