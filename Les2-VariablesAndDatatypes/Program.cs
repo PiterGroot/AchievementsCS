57db973b2db87aee3ddc28e16e73a1d826d9c6f8
PiterGroot <70685433+PiterGroot@users.noreply.github.com>
2021-03-09 14:49:44 +0100

    achievement

---
+++ Les2-VariablesAndDatatypes/Program.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace VarAndData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 10;
+            float number1 = number;
+            Console.WriteLine(number1);
+        }
+        
+    }
+}
