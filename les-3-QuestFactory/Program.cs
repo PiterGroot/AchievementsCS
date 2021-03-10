46e582fa6c45dc993a8e44cb8109a7cb1d790102
PiterGroot <70685433+PiterGroot@users.noreply.github.com>
2021-03-10 14:16:36 +0100

    achievem,tnm

---
+++ les-3-QuestFactory/Program.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs
+{
+    //bronze
+    class Program
+    {
+        public enum QuestType
+        {
+            GoblinDungeon,
+            WarriorTavern,
+            Garden
+        }
+
+        public static class QuestFactory
+        {
+            public static Quest CreateQuest(QuestType questType)
+            {
+                // -- Here are the if statements to replace with a switch statement --
+                switch (questType)
+                {
+                    case QuestType.GoblinDungeon:
+                        return new Quest("Goblin Dungeon", "A dungeon full of goblins.. Please help us traveler!");
+                    case QuestType.WarriorTavern:
+                        return new Quest("Warrior's Tarvern", "A warrior who owns a tarvern has reached out to you. Visit him.");
+                    case QuestType.Garden:
+                        return new Quest("Rose Garden", "The roses are wonderful.. but smelling them will change your fate..");
+                    default:
+                        Console.WriteLine("No Quest found for type " + questType);
+                        return null;
+                }
+            }
+        }
+        public class Quest
+        {
+            public string Title
+            {
+                get; private set;
+            }
+
+            public string Description
+            {
+                get; private set;
+            }
+
+            public Quest(string title, string description)
+            {
+                Title = title;
+                Description = description;
+            }
+        }
+    }
+}
